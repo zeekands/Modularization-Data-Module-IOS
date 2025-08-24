1fd4582bee06011b51fe5e6747cc1d73e13b7ca1
zeekands <azizkandias11@gmail.com>
2025-08-24 13:23:44 +0700

    change dependency name1

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     // Ini adalah dependensi eksternal dari pihak ketiga
     .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.1")),
     .package(url: "https://github.com/realm/realm-swift.git", .upToNextMajor(from: "20.0.3")),
-    .package(url: "https://github.com/zeekands/Modularization-Domain-Module-IOS.git"),
+    .package(url: "https://github.com/zeekands/Modularization-Domain-Module-IOS.git", from:"1.0.0"),
   ],
   targets: [
     .target(
@@ -25,7 +25,7 @@ let package = Package(
       dependencies: [
         .product(name: "Alamofire", package: "Alamofire"),
         .product(name: "RealmSwift", package: "realm-swift"),
-        .product(name: "SharedDomain", package: "Modularization-Domain-Module-IOS"),
+        "SharedDomain",
       ]
     ),
     .testTarget(
